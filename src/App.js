9662f0c195c0bc599e6963a372cec9233a2cb8cf
viola <pumpure@yahoo.com>
2023-07-18 11:03:18 -0600

    left panel project list added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import TopBar from "./components/TopBar/TopBar";
+import ProjectList from "./components/ProjectList/ProjectList";
 
 function App() {
   return (
-    <div className="App">
-      <TopBar placeholder="Search for a task..." />
+    <div>
+      <div className="TopBar">
+        <TopBar placeholder="Search for a task..." />
+      </div>
+      <div className="ProjectList">
+        <ProjectList />
+      </div>
     </div>
   );
 }
---
+++ src/components/ProjectList/ProjectList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ProjectList.css";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list">
+      <h1>Projects</h1>
+      <ul className="menu">
+        <li>Home</li>
+        <li>Work</li>
+        <li>Personal</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectList;
