91b73d17fcbd04bf998b212a746b5cee7a9a9790
viola <pumpure@yahoo.com>
2023-07-20 08:32:58 -0600

    search funcionality

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./TodoList.css";
-import { useEffect } from "react";
 import InputForm from "../InputForm/InputForm";
-
-
+import TopBar from "../TopBar/TopBar";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -13,6 +11,12 @@ const TodoList = () => {
     input: "",
     isChecked: false,
   });
+
+  // new state variable for search term
+  const [search, setSearch] = useState("");
+  const handleSearch = (term) => {
+    setSearch(term);
+  };
 
   const handleOnClick = () => {
     setTodos([...todos, text]);
@@ -37,20 +41,32 @@ const TodoList = () => {
     <h2>Loading...</h2>;
   }
 
+  //todos array filter
+  const filteredTodos = todos.filter(
+    (todo) =>
+      todo.input && todo.input.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
       <h3>REMAINING TASKS: {remainingItems}</h3>
-     
+
       <input
         onChange={(event) => setText({ ...text, input: event.target.value })}
         type="text"
       />
       <br />
       <button onClick={handleOnClick}>ADD TASK</button>
-     
 
-      <InputForm todos={todos} setTodos={setTodos} />
-     
+      {/* rendering TopBar with props */}
+      <TopBar
+        placeholder="Search for a task"
+        data={filteredTodos}
+        onSearch={handleSearch}
+      />
+      {/* passing filtered Todos list */}
+      <InputForm todos={filteredTodos} setTodos={setTodos} />
+
       <button
         onClick={() => {
           const remainingTodos = todos.filter((todo) => !todo.isChecked);
@@ -59,7 +75,6 @@ const TodoList = () => {
       >
         CLEAR LIST
       </button>
-     
     </div>
   );
 };
--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -4,16 +4,23 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 import "./TopBar.css";
 
-const TopBar = ({ placeholder, data }) => {
+const TopBar = ({ placeholder, data, onSearch }) => {
+  console.log("onSearch prop value:", onSearch);
   return (
     <div className="search">
       <div className="searchInput">
-        <input type="text" placeholder={placeholder} />
+        <input
+          type="text"
+          placeholder={placeholder}
+          onChange={(event) => onSearch(event.target.value)}
+        />
         <button className="searchButton" type="submit">
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
       </div>
-      <div className="searchResults"></div>
+      <div className="searchResults">
+        {data && data.map((item) => <p>{item.input}</p>)}
+      </div>
     </div>
   );
 };
